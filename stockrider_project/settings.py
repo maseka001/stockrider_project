55927c5a73d505157ea9eeaaf8f60c6079b7a432
Maseka <maseka@Maseka>
2026-03-07 16:06:33 +0300

    removed database credentials

--- stockrider_project/settings.py
+++ stockrider_project/settings.py
@@ -63,17 +63,18 @@ WSGI_APPLICATION = 'stockrider_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stockrider_db',
-        'USER': 'maseka',
-        'PASSWORD': 'passkey379',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+import os
 
+DATABASES = {
+ 'default': {
+  'ENGINE': 'django.db.backends.postgresql',
+  'NAME': os.getenv("DB_NAME"),
+  'USER': os.getenv("DB_USER"),
+  'PASSWORD': os.getenv("DB_PASSWORD"),
+  'HOST': os.getenv("DB_HOST"),
+  'PORT': os.getenv("DB_PORT"),
+ }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
